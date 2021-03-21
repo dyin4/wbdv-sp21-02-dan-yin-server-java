47543b6266ff20fa062061657706de93910a0980
Dan Yin <danyin@Dans-MacBook-Pro.local>
2021-03-20 19:38:12 -0700

    add widget class attributes

--- wbdv-sp21-02-dan-yin-server-java/src/main/java/com/example/wbdvsp2102danyinserverjava/models/Widget.java
+++ wbdv-sp21-02-dan-yin-server-java/src/main/java/com/example/wbdvsp2102danyinserverjava/models/Widget.java
@@ -8,6 +8,80 @@ public class Widget {
   private Integer size;
   private String text;
 
+  private String name;
+  private Integer widgetOrder;
+  private String url;
+  private Integer width;
+  private Integer height;
+  private String cssClass;
+  private String style;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getWidgetOrder() {
+    return widgetOrder;
+  }
+
+  public void setWidgetOrder(Integer widgetOrder) {
+    this.widgetOrder = widgetOrder;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Integer getWidth() {
+    return width;
+  }
+
+  public void setWidth(Integer width) {
+    this.width = width;
+  }
+
+  public Integer getHeight() {
+    return height;
+  }
+
+  public void setHeight(Integer height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  private String value;
+
   public Widget(Long id, String topicId, String type, Integer size, String text) {
     this.id = id;
     this.topicId = topicId;
